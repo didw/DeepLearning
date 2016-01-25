b550341ed14889d9dbc4a3af53885a175abf4b7d
Jongyeol Yang <yangjy0113@gmail.com>
2016-01-25 08:35:59 +0000

    add comments

--- torch/practical/practical4/requ.lua
+++ torch/practical/practical4/requ.lua
@@ -5,7 +5,8 @@ local ReQU = torch.class('nn.ReQU', 'nn.Module')
 function ReQU:updateOutput(input)
   -- TODO
   self.output:resizeAs(input):copy(input)
-  -- ...something here...
+  -- z = f(x) = x^2 if x > 0
+  --            0   otherwise
   self.output[torch.lt(input,0)] = 0
   self.output:pow(2)
   return self.output
@@ -14,7 +15,7 @@ end
 function ReQU:updateGradInput(input, gradOutput)
   -- TODO
   self.gradInput:resizeAs(gradOutput):copy(gradOutput)
-  -- ...something here...
+  -- dz_dx = 2x
   self.gradInput:cmul(input)
   self.gradInput:mul(2)
   return self.gradInput
