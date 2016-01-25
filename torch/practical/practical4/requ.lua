bb40d90adca470b9ba4819d042ea5fe5717e1d23
양종열 <jy.yang@lge.com>
2016-01-25 11:36:19 +0900

    implement requ

--- torch/practical/practical4/requ.lua
+++ torch/practical/practical4/requ.lua
@@ -5,7 +5,7 @@ local ReQU = torch.class('nn.ReQU', 'nn.Module')
 function ReQU:updateOutput(input)
   -- TODO
   self.output:resizeAs(input):copy(input)
-  -- ...something here...
+  self.output:cmul(self.output[torch.ge(self.output, 0)])
   return self.output
 end
 
@@ -13,6 +13,7 @@ function ReQU:updateGradInput(input, gradOutput)
   -- TODO
   self.gradInput:resizeAs(gradOutput):copy(gradOutput)
   -- ...something here...
+  self.gradInput:cmul(2*input)
   return self.gradInput
 end
 
